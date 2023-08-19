b964b056cba6b72688b7e4bbe9db96db8336879a
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-08-19 13:47:44 +0300

    added create parser

--- src/Query/src/Parsers/CreateParser.cpp
+++ src/Query/src/Parsers/CreateParser.cpp
@@ -9,19 +9,62 @@ using namespace std::literals;
 
 namespace {
 
+/**
+ * @brief bring query to format: "content"
+ *
+ * @param query string representation of query in format: "create structure { content }"
+ */
+void cleanup(std::string_view& query)
+{
+    query.remove_prefix("create"sv.length());
+    ltrim(query);
+
+    if (!startsWithIgnoreCase(query, "structure"))
+    {
+        throw std::runtime_error("Missing structure keyword in query definition @ '"s + std::string(query) + "'"s);
+    }
+    query.remove_prefix("structure"sv.length());
+    ltrim(query);
+
+    if (query.front() != '{')
+    {
+        throw std::runtime_error("Missing '{' symbol");
+    }
+    query.remove_prefix(1);
+
+    if (query.back() != '}')
+    {
+        throw std::runtime_error("Missing '}' symbol");
+    }
+    query.remove_suffix(1);
+
+    trim(query);
+}
+
+Primitives::EStructureType extractType(std::string_view& query)
+{
+    const auto type = extractValue(query, "type");
+
+    return Primitives::StructureType::to_literal(std::string(type));
+}
+
 } // Anonymous namespace
 
-// TODO: implement matcher...
 QUERY_COULD_MATCH(Create)
 {
-    return false;
+    return startsWithIgnoreCase(query, "create");
 }
 
 QUERY_PARSER(Create)
 {
     QUERY_OBJECT(obj, Create);
 
-    // TODO: implement Create parser...
+    cleanup(query);
+
+    obj.name = extractIdentifier(query, "name");
+    obj.type = extractType(query);
+    obj.schema = extractIdentifier(query, "based_on");
+    obj.is_volatile = extractBoolean(query, "volatile");
 
     RETURN_QUERY_OBJECT;
 }
--- src/Query/test/CreateParserTest.cpp
+++ src/Query/test/CreateParserTest.cpp
@@ -11,12 +11,161 @@ using namespace std::literals;
 
 TEST(CreateParserTest, parseSuccess)
 {
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: table;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
 
+        const auto out = parseQuery(query).get<Create>();
+
+        const Create expected = Init::CreateInit{}
+            .name("StructureName")
+            .type(Primitives::EStructureType::Table)
+            .schema("SchemaName")
+            .is_volatile(true);
+
+        EXPECT_CREATE_EQ(out, expected);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: document;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
+
+        const auto out = parseQuery(query).get<Create>();
+
+        const Create expected = Init::CreateInit{}
+            .name("StructureName")
+            .type(Primitives::EStructureType::Document)
+            .schema("SchemaName")
+            .is_volatile(true);
+
+        EXPECT_CREATE_EQ(out, expected);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: document;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
+
+        const auto out = parseQuery(query).get<Create>();
+
+        const Create expected = Init::CreateInit{}
+            .name("StructureName")
+            .type(Primitives::EStructureType::Document)
+            .schema("SchemaName")
+            .is_volatile(true);
+
+        EXPECT_CREATE_EQ(out, expected);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: document;"
+            "   based_on: SchemaName;"
+            "   volatile: false;"
+            "}";
+
+        const auto out = parseQuery(query).get<Create>();
+
+        const Create expected = Init::CreateInit{}
+            .name("StructureName")
+            .type(Primitives::EStructureType::Document)
+            .schema("SchemaName")
+            .is_volatile(false);
+
+        EXPECT_CREATE_EQ(out, expected);
+    }
+    {
+        const auto query =
+            "create           structure    {"
+            "   name   :    StructureName   ;  "
+            "   type  : table   ;  "
+            "   based_on  : SchemaName  ;  "
+            "   volatile  :   true  ;  "
+            "  }   ";
+
+        const auto out = parseQuery(query).get<Create>();
+
+        const Create expected = Init::CreateInit{}
+            .name("StructureName")
+            .type(Primitives::EStructureType::Table)
+            .schema("SchemaName")
+            .is_volatile(true);
+
+        EXPECT_CREATE_EQ(out, expected);
+    }
 }
 
 TEST(CreateParserTest, parseThrow)
 {
-
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: table;"
+            "   based_on: SchemaName;"
+            "   volatile: maybe;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
+    {
+        const auto query =
+            "create {"
+            "   name: StructureName;"
+            "   type: table;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   type: table;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   based_on: SchemaName;"
+            "   volatile: true;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: table;"
+            "   volatile: true;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
+    {
+        const auto query =
+            "create structure {"
+            "   name: StructureName;"
+            "   type: table;"
+            "   based_on: SchemaName;"
+            "}";
+        EXPECT_THROW({ parseQuery(query); }, std::runtime_error);
+    }
 }
 
 } // namespace Moonlight::QueryParser::Implementation::Tests
