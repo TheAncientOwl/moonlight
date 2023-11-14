5aa64186062da8c5c4e2560bc2671d88b48cbe2a
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-11-14 21:08:03 +0200

    v2 progress

---
+++ syntax/moonlight.v2.sql
@@ -0,0 +1,168 @@
+default types:
+    String, Boolean, DateTime,
+    Integer, Integer8, Integer16, Integer32, Integer64,
+    Decimal, Decimal8, Decimal16, Decimal32, Decimal64
+
+-- <TYPES_DEMO>
+type Address {
+    street: String;
+    number: Integer;
+}
+
+type Exam {
+    name: String;
+    date: DateTime;
+    grade: Decimal;
+}
+
+type Person {
+    name: String;
+    year: Integer;
+    birth_date: DateTime;
+    address: Address;
+}
+
+type Student inherits Person {
+    some_field: String;
+    exams: ArrayOf<Exam>;
+}
+
+type Professor inherits Person {
+    salary: Decimal;
+}
+-- </TYPES_DEMO>
+
+-- <SELECT_DEMO>
+select from Professors
+    if (salary > 4000) then
+    fields [ name, birth_date, salary ];
+
+select from Students
+    if (exams.grade < 5 and lower(exams.name) like "*_advanced") then
+    fields [ name, year, exams.grade, exams.name ];
+
+select from Students::Exams
+    if (grade < 5) then
+    fields [ name, date, grade, Students.some_field, Students.name, Students.birth_date ];
+
+-- </SELECT_DEMO>
+
+-- 1. CREATE
+create table|document Professors based on Professor;
+create table|document Professors based on Professor volatile;
+
+-- 2. DROP
+drop structure Professors;
+drop structure Professors cascade;
+
+-- 3. MIGRATE
+migrate structure Professors to NewSchema;
+migrate structure Professors to NewSchema using [
+    old_fiel1 => new_field1,
+    old_fiel2 => new_field2
+];
+
+-- 4. TRUNCATE
+truncate structure Professors;
+
+-- 5. RENAME
+rename structure|field|database from OldName to NewName;
+rename structure|field|database from Structure.OldFieldName to Structure.NewFieldName;
+
+-- 6. SELECT
+select from StructureName
+    if (rid = 11 or (rid >= 2 and 5 <= rid or some_field < 5000) or rid = 9  or rid = 120) then
+    fields [ field1, field2, ..., fieldx ]
+    order by [ field1 asc, field2 desc, ..., field_z desc ];
+
+-- 7. INSERT
+insert into Professors values [
+    Professor { 
+        salary: 4000,
+        Person {
+            name: "Bob",
+            birth_date: "09/10/1985",
+            address: Address {
+                street: "Some Street",
+                number: 5
+            }
+        }
+    },
+    -- OR
+    { 4500, { "Bob", "09/10/1985", { "Some Street", 5 } } },
+    -- OR
+    Professor {
+        salary: 4000,
+        rank: "HeadmasterSupreme", -- observe new field added dynamically
+        Person {
+            name: "Ultimate Bob",
+            birth_date: "09/10/1985",
+            address: Address {
+                street: "Some Street",
+                number: 5
+            }
+        }
+    },
+    -- OR
+    Professor {
+        salary: 4000,
+        rank(String): "HeadmasterSupreme", -- observe new field added dynamically with data type
+        Person {
+            name: "Ultimate Bob",
+            birth_date: "09/10/1985",
+            address: Address {
+                street: "Some Street",
+                number: 5
+            }
+        }
+    }
+];
+
+-- 8. UPDATE
+update structure Professors 
+    if (rid = 11 or (rid >= 2 and 5 <= rid or some_field < 5000) or rid = 9  or rid = 120) then
+    modify: [
+        field1 => field1 * 1.5 + 2,
+        field2 => field3
+    ]
+
+-- 9. DELETE
+delete from structure Professor
+    if (rid = 11 or (rid >= 2 and 5 <= rid or some_field < 5000) or rid = 9  or rid = 120);
+
+-- 10. LOCK
+set concurrency on structure Professor on|off;
+
+-- 12. GRANT
+grant [ update, insert, ..., delete ]
+    to UserName
+    on StructureName;
+
+-- 13. REVOKE
+revoke [ update, insert, ..., delete ]
+    from UserName
+    on StructureName;
+
+-- 14. COMMIT
+commit;
+
+-- 15. ROLLBACK
+rollback;
+
+-- 16. SAVEPOINT
+savepoint;
+savepoint SavepointName;
+
+-- 17. INDEX
+create unique index IndexName on StructureName;
+create unique index IndexName on StructureName using [
+    field1, field2, ..., fieldx
+];
+
+-- 18. DATABASE
+database create|drop|backup DatabaseName 
+    to disk "/home/user/lunardb-backup"
+    with differential;
+
+-- 19. VIEW
+create view ViewName...
